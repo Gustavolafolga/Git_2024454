99f228301233b38a4648b5dfbd4701dbcb649f6b
Gustavolafolga <Gustavo.pradolafolga@gmail.com>
2024-11-27 16:39:36 +0000

    New Project to test git

---
+++ src/main/java/com/mycompany/git_2024454/Git_2024454.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.git_2024454;
+
+/**
+ *
+ * @author Chrystian
+ */
+public class Git_2024454 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
